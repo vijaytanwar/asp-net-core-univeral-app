b33f36e2d1cec9fa98faf1e0d67110c147eba9ad
Vijay <vsingh123@sapient.com>
2018-06-26 20:47:30 +0530

    Initial Commit

---
+++ WebApplication1/Helpers/MyViewLocationExpander.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Helpers
+{
+    public class MyViewLocationExpander : IViewLocationExpander
+    {
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context,
+            IEnumerable<string> viewLocations)
+        {
+
+            //replace the Views to MyViews..  
+            List<string> locations = new List<string>(viewLocations);
+
+            locations.Add("/Templates/{0}.cshtml");
+            return locations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            //nothing to do here.  
+        }
+    }
+}
---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    let page_container = document.querySelector("#Page_Container");
+    fetch("./generated_template_file.html").then((response) => {
+        // When the page is loaded convert it to text
+        return response.text()
+    }).then((html) => {
+        document.querySelector("#templateHolder").innerHTML = html;
+
+        document.addEventListener("click", (e) => {
+            if (e.target.tagName == "A") {
+                window.history.pushState(null, null, e.target.href);
+                let chunks = e.target.href.toLowerCase().split("/");
+                let fileName = "/" + (chunks[chunks.length - 1] === "" ? "index" : chunks[chunks.length - 1]);
+                let template = document.getElementById(fileName).innerHTML;
+                let str = razor.render(template, { model: { Title: "Index", Message: "Hello" } });
+                page_container.innerHTML = str.replace(/~\//g, "");
+                e.preventDefault();
+                return false;
+            }
+        })
+    });
+}); 
